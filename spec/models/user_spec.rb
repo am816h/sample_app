cc76c3f67a0d800c3f29e63fbad9f8d703d9d6aa
am816h <am816h@gmail.com>
2015-09-05 10:17:24 +0000

    Complete excercises

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(
+      name: "Example User",
+      email: "user@example.com",
+      password: "foobar",
+      password_confirmation: "foobar"
+    )
   end
 
   subject { @user }
@@ -34,8 +39,14 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[
+        user@foo,com
+        user_at_foo.org
+        example.user@foo.
+        foo@bar_baz.com
+        foo@bar+baz.com
+	foo@bar..com
+      ]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -45,7 +56,12 @@ describe User do
 
   describe "when email format is valid" do
     it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses = %w[
+        user@foo.COM
+        A_US-ER@f.b.org
+        frst.lst@foo.jp
+        a+b@baz.cn
+      ]
       addresses.each do |valid_address|
         @user.email = valid_address
         expect(@user).to be_valid
@@ -58,7 +74,6 @@ describe User do
       user_with_same_email = @user.dup
       user_with_same_email.save
     end
-
     it { should_not be_valid }
   end
 
@@ -68,13 +83,26 @@ describe User do
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
-
     it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 
   describe "when password is not present" do
     before do
-      @user = User.new(name: "Example User", email: "user@example.com", password: " ", password_confirmation: " ")
+      @user = User.new(
+        name: "Example User",
+	email: "user@example.com",
+	password: " ",
+	password_confirmation: " "
+      )
     end
     it { should_not be_valid }
   end
@@ -94,7 +122,6 @@ describe User do
 
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
