ae3a971c74eec57046fb4f7857683e5dfc355beb
Hayashi Yasukuni <hayashi@Hayashi-no-MacBook-Pro.local>
2015-11-03 16:57:21 +0900

    Complete excercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -67,6 +67,15 @@ describe "User pages" do
     it { should have_title(full_title('Sign up')) }
   end
 
+  describe "signup page after signed in" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit signup_path
+    end
+    it { should have_title(full_title("")) }
+  end
+
   describe "signup" do
 
     before { visit signup_path }
@@ -134,7 +143,7 @@ describe "User pages" do
         fill_in "Name", with: new_name
         fill_in "Email", with: new_email
         fill_in "Password", with: user.password
-        fill_in "Confirm Password", with: user.password
+        fill_in "Confirm", with: user.password
         click_button "Save changes"
       end
 
@@ -143,6 +152,17 @@ describe "User pages" do
       it { should have_link('Sign out', href: signout_path) }
       specify { expect(user.reload.name).to eq new_name }
       specify { expect(user.reload.email).to eq new_email }
+    end
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password, password_confirmation: user.password } }
+      end
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
     end
   end
 end
